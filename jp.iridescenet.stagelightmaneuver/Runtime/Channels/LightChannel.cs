9be359fcc32010e7460ee7c5ffd9074c6b12a800
pon <49616225+AJpon@users.noreply.github.com>
2024-01-26 22:52:39 +0900

    VLBの更新タイミングをLightChannelの更新タイミングに同期するように変更

--- jp.iridescenet.stagelightmaneuver/Runtime/Channels/LightChannel.cs
+++ jp.iridescenet.stagelightmaneuver/Runtime/Channels/LightChannel.cs
@@ -231,6 +231,30 @@ namespace StageLightManeuver
 #endif
 
 #if USE_VLB
+                // VLB のプロパティをライトに合わせて更新
+                //! BeamSD 動作未チェック 
+                //TODO: 動作確認
+                if (volumetricLightBeamHd )
+                {
+                    // VLB.VolumetricLightBeamHD.AssignPropertiesFromAttachedSpotLight の実装を参照
+                    // volumetricLightBeamHd.colorMode = ColorMode.Flat;
+                    // volumetricLightBeamHd.colorFlat = lightColor;
+
+                    // volumetricLightBeamHd.AssignPropertiesFromAttachedSpotLight();
+                    
+                    // UpdateAfterManualPropertyChange 内で AssignPropertiesFromAttachedSpotLight が呼ばれる
+                    volumetricLightBeamHd.UpdateAfterManualPropertyChange();
+                } 
+                else if (volumetricLightBeamSd && volumetricLightBeamSd.trackChangesDuringPlaytime)
+                {
+                    // VLB.VolumetricLightBeamSD.AssignPropertiesFromAttachedSpotLight の実装を参照
+                    // volumetricLightBeamSd.colorMode = ColorMode.Flat;
+                    // volumetricLightBeamSd.color = lightColor; 
+
+                    // UpdateAfterManualPropertyChange 内で AssignPropertiesFromAttachedSpotLight が呼ばれる
+                    volumetricLightBeamSd.UpdateAfterManualPropertyChange();
+                }
+                
                 if (volumetricCookieHd && !ignoreLightCookie)
                 {
                     volumetricCookieHd.cookieTexture = lightCookie;

3396177ed16058648cfce59775dc89547c8e5898
pon <49616225+AJpon@users.noreply.github.com>
2024-04-21 22:10:09 +0900

    Fixture コンポーネントのUIを変更

--- jp.iridescenet.stagelightmaneuver/Editor/CustomEditor/MovingStageLightEditor.cs
+++ jp.iridescenet.stagelightmaneuver/Editor/CustomEditor/MovingStageLightEditor.cs
@@ -23,67 +23,15 @@ namespace StageLightManeuver
             // return base.CreateInspectorGUI();
             
             _targetStageLightFixture = target as StageLightFixture;
-            var indexField = new PropertyField(serializedObject.FindProperty("index"));
-            indexField.SetEnabled(false); 
-            root.Add(indexField);
-            root.Add(new PropertyField(serializedObject.FindProperty("stageLightChannels")));
-            channelList = new List<string>();
-            channelList.Add("Add New Channel");
-          
-           
 
-            Init();
-
-            var center = new VisualElement();
-            center.style.alignItems = Align.Center;
-            var popupField = new PopupField<string>(channelList, 0);
-            popupField.SetEnabled( channelList.Count > 1 );
-            popupField.RegisterValueChangedCallback((evt =>
-            {
-                if (popupField.index != 0)
-                {
-                    var type = GetTypeByClassName(popupField.value);
-                    MethodInfo mi = typeof(GameObject).GetMethod(
-                        "AddComponent",
-                        BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance,
-                        null,
-                        new Type[0],
-                        null
-                    );
-                    MethodInfo bound = mi.MakeGenericMethod(type);
-                    
-                    foreach (var obj in targets) //! Add Channel to all Fixtures when multiple selections are made
-                    {
-                        var stageLightFixture = obj as StageLightFixture;
-                        if (stageLightFixture == null) continue;
-                        var channel =bound.Invoke(stageLightFixture.gameObject, null) as StageLightChannelBase;
-                        if(channel)channel.Init();
-                        stageLightFixture.FindChannels();
-                    }
-                }
-            }));
-            center.Add(popupField);
-            root.Add(center);
-            root.Add(new PropertyField(serializedObject.FindProperty("syncStageLight")));
-
-            // ----- Fixture Profile
-
-            // 水平線を追加
-            root.Add(new IMGUIContainer(() =>
-            {
-                EditorGUILayout.Space();
-                EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
-                // EditorGUILayout.LabelField("Fixture Profile", EditorStyles.boldLabel);
-
-            }));
-
+             // ----- Fixture Profile -----
             root.Add(new PropertyField(serializedObject.FindProperty("lightFixtureProfile"), "Fixture Profile"));
             // lightFixtureProfile がセットされているか
-            bool isProfileSet = _targetStageLightFixture.lightFixtureProfile != null;
+            // bool isProfileSet = _targetStageLightFixture.lightFixtureProfile != null;
 
             var horizontal = new VisualElement();
             horizontal.style.flexDirection = FlexDirection.Row;
-            horizontal.style.paddingBottom = 4;
+            // horizontal.style.paddingBottom = 4;
             horizontal.style.justifyContent = Justify.FlexEnd;
 
             horizontal.Add(new Button(() =>
@@ -127,6 +75,57 @@ namespace StageLightManeuver
             root.Add(horizontal);
 
             // -----
+            // 水平線を追加
+            root.Add(new IMGUIContainer(() =>
+            {
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+                // EditorGUILayout.LabelField("Fixture Profile", EditorStyles.boldLabel);
+
+            }));
+
+            var indexField = new PropertyField(serializedObject.FindProperty("index"));
+            indexField.SetEnabled(false); 
+            root.Add(indexField);
+            root.Add(new PropertyField(serializedObject.FindProperty("stageLightChannels")));
+            channelList = new List<string>();
+            channelList.Add("Add New Channel");
+          
+           
+
+            Init();
+
+            var center = new VisualElement();
+            center.style.alignItems = Align.Center;
+            var popupField = new PopupField<string>(channelList, 0);
+            popupField.SetEnabled( channelList.Count > 1 );
+            popupField.RegisterValueChangedCallback((evt =>
+            {
+                if (popupField.index != 0)
+                {
+                    var type = GetTypeByClassName(popupField.value);
+                    MethodInfo mi = typeof(GameObject).GetMethod(
+                        "AddComponent",
+                        BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance,
+                        null,
+                        new Type[0],
+                        null
+                    );
+                    MethodInfo bound = mi.MakeGenericMethod(type);
+                    
+                    foreach (var obj in targets) //! Add Channel to all Fixtures when multiple selections are made
+                    {
+                        var stageLightFixture = obj as StageLightFixture;
+                        if (stageLightFixture == null) continue;
+                        var channel =bound.Invoke(stageLightFixture.gameObject, null) as StageLightChannelBase;
+                        if(channel)channel.Init();
+                        stageLightFixture.FindChannels();
+                    }
+                }
+            }));
+            center.Add(popupField);
+            root.Add(center);
+            root.Add(new PropertyField(serializedObject.FindProperty("syncStageLight")));
 
             return root;
         }

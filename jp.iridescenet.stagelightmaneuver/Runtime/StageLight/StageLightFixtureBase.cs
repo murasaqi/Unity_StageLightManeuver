51d8d6da358fce2c67feb13b81f0ff56c25f255b
murasaqi <murasaqi@iridescent.jp>
2025-05-04 16:00:52 +0900

    MeshのBoundingBoxをDrawiするように最適化

--- jp.iridescenet.stagelightmaneuver/Runtime/StageLight/StageLightFixtureBase.cs
+++ jp.iridescenet.stagelightmaneuver/Runtime/StageLight/StageLightFixtureBase.cs
@@ -36,15 +36,36 @@ namespace StageLightManeuver
         private void OnDrawGizmos()
         {
             if (!showGizmo) return;
-
+        
             Gizmos.color = gizmoColor;
             foreach (var fixture in stageLightFixtures)
             {
                 if (fixture != null)
                 {
-                    Gizmos.DrawWireCube(fixture.transform.position, gizmoSize);
+                    DrawBoundCube(fixture.transform);
+                    // Gizmos.DrawWireCube(fixture.transform.position, gizmoSize);
                 }
             }
+        }
+        
+        void DrawBoundCube(Transform transform)
+        {
+            var renderers = transform.GetComponentsInChildren<Renderer>();
+            Vector3 min = Vector3.one*float.MaxValue, max = Vector3.one*float.MinValue;
+            int count = 0;
+            foreach (var renderer in renderers)
+            {
+                if (renderer == null) continue;
+                var bounds = renderer.bounds;
+                min = Vector3.Min(min, bounds.min);
+                max = Vector3.Max(max, bounds.max);
+                count++;
+            }
+        
+            if(count > 0)
+                Gizmos.DrawWireCube((min+max)/2, max-min);
+            else
+                Gizmos.DrawWireCube(transform.position, gizmoSize);
         }
 
         [ContextMenu("Toggle Gizmo")]
